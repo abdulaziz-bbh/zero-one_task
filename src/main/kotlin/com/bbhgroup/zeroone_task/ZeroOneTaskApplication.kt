89b5940510ad0180e696e1a66cebc2458a35f11c
abdulaziz_bbh <abdulaziz.hikmatov2001@gmail.com>
2024-12-05 01:45:42 +0500

    anything_2

--- src/main/kotlin/com/bbhgroup/zeroone_task/ZeroOneTaskApplication.kt
+++ src/main/kotlin/com/bbhgroup/zeroone_task/ZeroOneTaskApplication.kt
@@ -2,8 +2,10 @@ package com.bbhgroup.zeroone_task
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl::class)
 class ZeroOneTaskApplication
 
 fun main(args: Array<String>) {
